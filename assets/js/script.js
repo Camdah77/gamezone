c32e2d890f542543e64b23967ff70c62702647b1
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-10-06 11:10:39 +0000

    JS: Card Array created for choosing cards

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+//Array with the game-cards
+const items =[
+    {name:"Memo1", image:"./assets/images/mem1.webp" }
+
+]
